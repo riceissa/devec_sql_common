e47517db9690aa0c91915d6d775ae8817ce302e0
Issa Rice <riceissa@gmail.com>
2017-11-22 17:53:05 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# Using https://python-packaging.readthedocs.io/en/latest/minimal.html
+# as a guide.
+
+setup(name='devec_sql_common',
+      version='0.1',
+      description='Common utilities for working with SQL generation for Devec',
+      url='https://github.com/riceissa/devec_sql_common',
+      author='Issa Rice',
+      author_email='riceissa@gmail.com',
+      license='CC0',
+      packages=['devec_sql_common'],
+      zip_safe=False)
